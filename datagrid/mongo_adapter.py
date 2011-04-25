5ffcad051ba2db579e95cfb6b28e1a5e783776d9
Ilia Vihtinsky <vihtinsky@yandex.ru>
2011-04-25 14:18:06 +0400

    Multi sort with mongo adapter

--- datagrid/mongo_adapter.py
+++ datagrid/mongo_adapter.py
@@ -3,23 +3,23 @@ import pymongo
 import pymongo.cursor
 import logging
 
-from adapters import QuerySetAdapter, ManagerAdapter, Struct
+from adapters import QuerySetAdapter, ManagerAdapter
 
 
 class MongoQuerySetAdapter(QuerySetAdapter):
     """Decorator to use mongo query with datagrid"""
-    def __init__(self, mongo_cursor, id = "id"):
+    def __init__(self, mongo_cursor, pk = "id"):
         if not isinstance(mongo_cursor, pymongo.cursor.Cursor):
             raise Exception("Argument should be pymongo.cursor.Cursor")
         self.model = ManagerAdapter()
         self.model.objects = self
-        self.id = id
+        self.pk = pk
         self.mongo_cursor = mongo_cursor
 
     def __getitem__(self, items):
         if isinstance(items,int):
             i = self.mongo_cursor[items]
-            return Struct(**i)
+            return Struct(self.pk, **i)
         self.mongo_cursor = self.mongo_cursor.__getitem__(items)
         return self
 
@@ -27,11 +27,11 @@ class MongoQuerySetAdapter(QuerySetAdapter):
         return self
 
     def filter_pk(self, ids_list):
-        id = self.id
-        code = "%s.indexOf[this.%s] != -1 " % (ids_list, id)
-
+        pk = self.pk
+        code = "%s.indexOf[this.%s] != -1 " % (ids_list, pk)
         mongo_cursor = self.mongo_cursor.where(code)
-        a = [Struct(**i) for i in mongo_cursor]
+        print code
+        a = [Struct(self.pk, **i) for i in mongo_cursor]
         return a
 
     def count(self):
@@ -46,7 +46,7 @@ class MongoQuerySetAdapter(QuerySetAdapter):
         if fields:
             field = fields[0]
             if field == "pk":
-                field = self.id
+                field = self.pk
         else:
             return self
         cursor = copy.copy(self.mongo_cursor)
@@ -59,18 +59,36 @@ class MongoQuerySetAdapter(QuerySetAdapter):
         return self.mongo_cursor.count()
 
     def order_by(self, *field_names):
+        print field_names
+        asc = pymongo.ASCENDING
+        desc = pymongo.DESCENDING
+        sort = []
         if not field_names:
             return self
-        index = field_names[0]
-        reverse = pymongo.ASCENDING
-        if index.startswith("-"):
-            reverse = pymongo.DESCENDING
-            index = index[1:]
-        self.mongo_cursor = self.mongo_cursor.sort(index, direction=reverse)
+        for index in field_names:
+            if index.startswith("-"):
+                index = index[1:]
+                sort.append((index, desc))
+            else:
+                sort.append((index, asc))
+        self.mongo_cursor = self.mongo_cursor.sort(sort)
         return self
+
 
     def extra_sort(self, *field_names):
         logging.error("""Sort by nonDb column with MongoQuerySetAdapter
             not supported""")
         return self
 
+class Struct:
+    """Object presentation of dictionaries
+    >>> d = {'a+b': 'c', 'c': 3}
+    >>> s = Struct(**d)
+    >>> getattr(s,'a+b')
+    'c'
+    >>> s.c
+    3
+    """
+    def __init__(self, pk="id", **entries):
+        self.__dict__["id"] = entries[pk]
+        self.__dict__.update(entries)
